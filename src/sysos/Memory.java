4bb8095ee030b4a49120015081e07e9278e5bfe1
Qroshi <34942734+Qroshi@users.noreply.github.com>
2018-01-07 17:32:32 +0100

    Add files via upload

--- src/sysos/Memory.java
+++ src/sysos/Memory.java
@@ -1,5 +1,3 @@
-package sysos;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,6 +8,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Memory {
+
 	private char[] ram; // tablica zawierajaca pamiec
 	private File swap; // plik wymiany
 	public FFA ffa; // tablica wolnych ramek
@@ -23,52 +22,26 @@ public class Memory {
 			ram[i] = ' ';
 		ffa = new FFA();
 		swap = new File("swap.txt");
+		if (!swap.exists())
+			try {
+				swap.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		wipeSwap();
 		lruStack = new Stack<>();
 	}
 
-	////////////////////////////////// METODY /////////////////////////////////
-
-	public char readMemory(int l_addr) { // zwraca char z pamiï¿½ci
-		if (l_addr > getProgramSize()) {
-			System.out.println("ADRES LOGICZNY WIEKSZY NIZ ROZMIAR PROGRAMU");
-			return ' ';
-		}
-		int strona = l_addr / 16;
-		int ramka = Main.T.getPcb(Main.OBECNY_PROCES).pageCheck(strona);
-
-		if (ramka != -1) {
-			updateStack(ramka);
-			return ram[ramka * 16 + l_addr % 16];
-		}
-
-		if (isRamFull()) {
-			int victim = getVictim();
-			Main.T.getPcb(ffa.checkFrame(victim)).pageDisable(ffa.checkFrame(victim));
-			putPageToSwap(victim, Main.T.getPcb(ffa.checkFrame(victim)).swapFileBeginning * 16 + strona);
-			ffa.releaseFrame(victim);
-		}
-
-		int free = findFreeFrame();
-		String toPut = getPageFromSwap(Main.T.getPcb(Main.OBECNY_PROCES).swapFileBeginning + strona);
-		for (int i = free * 16, j = 0; j < 16 && j < toPut.length(); i++, j++) {
-			ram[i] = toPut.charAt(j);
-		}
-		Main.T.getPcb(Main.OBECNY_PROCES).pageEnable(strona, free);
-		ramka = Main.T.getPcb(Main.OBECNY_PROCES).pageCheck(strona);
-		ffa.occupyFrame(ramka, Main.OBECNY_PROCES);
-		updateStack(ramka);
-
-		int doZwrotu = ramka * 16 + l_addr % 16;
-		return ram[doZwrotu];
-	}
+	////////////////////////////////// PUBLICZNE///////////////////////////////////////////////////////////////////
 
 	public void writeMemory(int l_addr, char value) {
-		if (l_addr > getProgramSize()) {
+		PCB current = Main.T.getPcb(Main.OBECNY_PROCES);
+
+		if (l_addr > getProgramSize(current)) {
 			System.out.println("ADRES LOGICZNY WIEKSZY NIZ ROZMIAR PROGRAMU");
 		}
 		int strona = l_addr / 16;
-		int ramka = Main.T.getPcb(Main.OBECNY_PROCES).pageCheck(strona);
+		int ramka = current.pageCheck(strona);
 		if (ramka != -1) {
 			updateStack(ramka);
 			ram[ramka * 16 + l_addr % 16] = value;
@@ -81,21 +54,71 @@ public class Memory {
 			putPageToSwap(victim, Main.T.getPcb(ffa.checkFrame(victim)).swapFileBeginning * 16 + strona);
 			ffa.releaseFrame(victim);
 		}
-
 		int free = findFreeFrame();
-		String toPut = getPageFromSwap(Main.T.getPcb(Main.OBECNY_PROCES).swapFileBeginning + strona);
+		String toPut = getPageFromSwap(current.swapFileBeginning + strona);
 		for (int i = free * 16, j = 0; j < 16; i++, j++) {
 			ram[i] = toPut.charAt(j);
 		}
-		Main.T.getPcb(Main.OBECNY_PROCES).pageEnable(strona, free);
-		ramka = Main.T.getPcb(Main.OBECNY_PROCES).pageCheck(strona);
+		current.pageEnable(strona, free);
+		ramka = current.pageCheck(strona);
 		ffa.occupyFrame(ramka, Main.OBECNY_PROCES);
 		updateStack(ramka);
 
 		ram[ramka * 16 + l_addr % 16] = value;
 	}
 
-	public void putPageToSwap(int victim, int place) { // umieszcza stronï¿½ w podanym miejscu w pliku wymiany
+	public void allocateMemory(String program, int size) { // Wpisuje program do pliku wymiany
+		PCB pcb = Main.T.getPcb(Main.OBECNY_PROCES);
+		pcb.createPageTable(size);
+		pcb.swapFileBeginning = SWAP_END;
+		pcb.programSize = size;
+		SWAP_END += (size + 16) / 16;
+
+		String temp = new String();
+		try {
+			Scanner scan = new Scanner(swap);
+
+			temp = scan.nextLine();
+			scan.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(swap));
+			temp += program;
+			// int l;
+			// int ile = (size / 16) + 1;
+			// int fulls = ile - ((program.length() / 16) + 1);
+			// int dop = 16 - (program.length() % 16);
+			// l = (16 * fulls) + dop;
+			int l = (16 * (((size / 16) + 1) - ((program.length() / 16) + 1))) + 16 - (program.length() % 16);
+			for (int i = 0; i < l; i++) {
+				temp += ' ';
+			}
+			out.write(temp);
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deallocateMemory(int processID) { // zwalnia zasoby pamieci po zakonczeniu procesu
+		ffa.deallocate(processID);
+	}
+
+	public String readUntilSpace(int l_addr) { // zwraca string do spacji
+		String out = new String();
+		for (int i = 0; l_addr + i < getProgramSize(); i++) {
+			char temp = readMemory(l_addr + i);
+			if (temp == ' ')
+				break;
+			out += temp;
+		}
+		return out;
+	}
+
+	//////////////////////////////// METODY_MOJE///////////////////////////////////////////////////////
+	private void putPageToSwap(int victim, int place) { // umieszcza stronê w podanym miejscu w pliku wymiany
 		String toPut = new String();
 		for (int i = victim * 16; i < (victim * 16) + 16; i++) {
 			toPut += ram[i];
@@ -124,44 +147,7 @@ public class Memory {
 		}
 	}
 
-	public void allocateMemory(String program, int size) { // Wpisuje program do pliku wymiany
-		PCB pcb = Main.T.getPcb(Main.OBECNY_PROCES);
-		pcb.createPageTable(size);
-		pcb.swapFileBeginning = SWAP_END;
-		pcb.programSize = size;
-		SWAP_END += (size + 15) / 16;
-
-		String temp = new String();
-		try {
-			Scanner scan = new Scanner(swap);
-
-			temp = scan.nextLine();
-			scan.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			BufferedWriter out = new BufferedWriter(new FileWriter(swap));
-
-			temp += program;
-			int l = 16 - (program.length() % 16);
-			if (16 - (size % 16) > l)
-				l = 16 - (size % 16);
-			
-			for (int i = 0; i < l; i++) {
-				temp += ' ';
-			}
-			out.write(temp);
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	//////////////////////////////// GOTOWE /////////////////////////////
-	public String getPageFromSwap(int pageNumber) { // zwraca stronï¿½ o danym numerze z pliku wymiany
+	private String getPageFromSwap(int pageNumber) { // zwraca stronê o danym numerze z pliku wymiany
 		String out = new String();
 		try {
 			FileInputStream fis = new FileInputStream(swap);
@@ -179,7 +165,7 @@ public class Memory {
 		return out;
 	}
 
-	public int findFreeFrame() { // zwraca numer wolnej ramki
+	private int findFreeFrame() { // zwraca numer wolnej ramki
 		for (int i = 0; i < 8; i++) {
 			if (ffa.checkFrame(i) == -1)
 				return i;
@@ -187,15 +173,19 @@ public class Memory {
 		return -1;
 	}
 
-	public int getVictim() { // zwraca numer ofiary LRU
+	private int getVictim() { // zwraca numer ofiary LRU
 		return lruStack.firstElement();
 	}
 
-	public int getProgramSize() { // zwraca rozmiar obecnego programu
+	private int getProgramSize() { // zwraca rozmiar obecnego programu
 		return Main.T.getPcb(Main.OBECNY_PROCES).programSize;
 	}
 
-	public boolean isRamFull() { // sprawdza czy wszystkie ramki sa zajete
+	private int getProgramSize(PCB p) { // zwraca rozmiar programu o podanym PCB
+		return p.programSize;
+	}
+
+	private boolean isRamFull() { // sprawdza czy wszystkie ramki sa zajete
 		for (int i = 0; i < 8; i++) {
 			if (ffa.checkFrame(i) == -1) {
 				return false;
@@ -204,18 +194,7 @@ public class Memory {
 		return true;
 	}
 
-	public String readUntilSpace(int l_addr) { // zwraca string do spacji
-		String out = new String();
-		for (int i = 0; l_addr + i < getProgramSize(); i++) {
-			char temp = readMemory(l_addr + i);
-			if (temp == ' ')
-				break;
-			out += temp;
-		}
-		return out;
-	}
-
-	public void updateStack(int nr) { // aktualizuje stos LRU podanym numerem ramki
+	private void updateStack(int nr) { // aktualizuje stos LRU podanym numerem ramki
 		int position = lruStack.search(nr);
 		if (position != -1) {
 			lruStack.remove(lruStack.size() - position);
@@ -224,7 +203,7 @@ public class Memory {
 			lruStack.push(nr);
 	}
 
-	public void wipeSwap() { // zeruje plik wymiany i umieszcza kod bezczynnosci
+	private void wipeSwap() { // zeruje plik wymiany i umieszcza kod bezczynnosci
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(swap));
 			out.write("1&&&&&&&3&&&&&&0"); // tutaj umiescic kod bezczynnosci
@@ -232,6 +211,44 @@ public class Memory {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public char readMemory(int l_addr) { // zwraca char z pamiêci
+		PCB current = Main.T.getPcb(Main.OBECNY_PROCES);
+
+		if (l_addr > getProgramSize(current)) {
+			System.out.println("ADRES LOGICZNY WIEKSZY NIZ ROZMIAR PROGRAMU");
+			return ' ';
+		}
+		int strona = l_addr / 16;
+		int ramka = current.pageCheck(strona);
+
+		if (ramka != -1) {
+			updateStack(ramka);
+			return ram[ramka * 16 + l_addr % 16];
+		}
+
+		if (isRamFull()) {
+			int victim = getVictim();
+			int f = ffa.checkFrame(victim);
+			PCB p = Main.T.getPcb(f);
+			p.pageDisable(victim);
+			putPageToSwap(victim, p.swapFileBeginning * 16 + strona);
+			ffa.releaseFrame(victim);
+		}
+
+		int free = findFreeFrame();
+		String toPut = getPageFromSwap(current.swapFileBeginning + strona);
+		for (int i = free * 16, j = 0; j < 16 && j < toPut.length(); i++, j++) {
+			ram[i] = toPut.charAt(j);
+		}
+		current.pageEnable(strona, free);
+		ramka = Main.T.getPcb(Main.OBECNY_PROCES).pageCheck(strona);
+		ffa.occupyFrame(ramka, Main.OBECNY_PROCES);
+		updateStack(ramka);
+
+		int doZwrotu = ramka * 16 + l_addr % 16;
+		return ram[doZwrotu];
 	}
 	//////////////////////////// TESTOWE ////////////////////////////////////
 
@@ -259,5 +276,56 @@ public class Memory {
 			else
 				System.out.println("P" + i + ": occupied");
 		}
+	}
+	////////////////////// TABLICA_WOLNYCH_RAMEK/////////////////////////////////
+
+	private class FFA { // Free Frames Array, Tablica wolnych ramek
+
+		private class Pair {
+			public boolean isFree = true;
+			public int processID = -1;
+		}
+
+		public Pair[] ffa = new Pair[8];
+
+		public FFA() {
+			for (int i = 0; i < 8; i++) {
+				ffa[i] = new Pair();
+			}
+		}
+
+		public int checkFrame(int which) {
+			if (ffa[which].isFree)
+				return -1;
+			else
+				return ffa[which].processID;
+		}
+
+		public boolean isFree(int which) {
+			return ffa[which].isFree;
+		}
+
+		public void occupyFrame(int which, int processID) {
+			if (ffa[which].isFree) {
+				ffa[which].isFree = false;
+				ffa[which].processID = processID;
+			} else
+				System.out.println("RAMKA ZAJETA");
+		}
+
+		public void releaseFrame(int which) {
+			ffa[which].isFree = true;
+			ffa[which].processID = -1;
+		}
+
+		public void deallocate(int id) {
+			for (int i = 0; i < 8; i++) {
+				if (ffa[id].processID == id) {
+					ffa[id].isFree = true;
+				}
+			}
+
+		}
+
 	}
 }
