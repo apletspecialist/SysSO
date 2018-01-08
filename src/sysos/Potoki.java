86125750be05d55700a1c1c354e161af09974cb2
Wojciech Niedbala <wojdna@gmail.com>
2018-01-08 17:27:39 +0100

    Update Potoki.java

--- src/sysos/Potoki.java
+++ src/sysos/Potoki.java
@@ -1,13 +1,5 @@
 package sysos;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package potoki;
-
 import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Queue;
