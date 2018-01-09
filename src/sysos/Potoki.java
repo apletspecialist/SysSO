9c9153d7fd9bb932f812aceb47145edcf4f77188
apletspecialist <35273481+apletspecialist@users.noreply.github.com>
2018-01-09 21:56:48 +0100

    Update Potoki.java

--- src/sysos/Potoki.java
+++ src/sysos/Potoki.java
@@ -3,7 +3,7 @@ package sysos;
 import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Queue;
-
+import sysos.process_manager.process;
 
 /**
  *
@@ -16,20 +16,21 @@ public class Potoki {
      */
     //struktura danych
     Queue<Character> myQueue = new LinkedList<Character>();
-    int qfreespace = 16;//max rozmiar na dane
+    int qfreespace = 256;//max rozmiar na dane
     int readbytes = 0;//zapisane bity
     int writebytes = 0;//odczytane bity
     int open = 0;//czy potok jest wykorzystywany przez potok
 
 //funkcje odczytu z potoku
-    public int read(process p) {
-        if (myQueue.peek() == null) {
+    static public int read(process p) {
+        int index=p.next.des;
+        if (plik1.tab[index].myQueue.peek() == null) {
             return 0;
         } else {
-            while (myQueue.peek() != null) {
-                p.next.IO.offer(myQueue.poll());
-                readbytes++;
-                qfreespace++;
+            while (plik1.tab[index].myQueue.peek() != null) {
+                p.next.IO.offer(plik1.tab[index].myQueue.poll());
+                plik1.tab[index].readbytes++;
+                plik1.tab[index].qfreespace++;
             }
             return 1;
         }
@@ -37,16 +38,18 @@ public class Potoki {
     }
 
     //funcja zapisu do potoku, zwraca 0 dla błędu 1 dla zapisania wszystkich info,2 dla przepałnienia 
-    public int write(process p) {
+   static public int write(process p) {
        Character znak ;//buffor znaku
-        if (qfreespace == 0) {
+       int index=p.des;
+        if (plik1.tab[index].qfreespace == 0) {
             return 0;
         } else {
             while (p.IO.peek() != null) {
+                plik1.tab[index].readbytes++;//zapisane bity
                 znak = p.IO.poll();
-                myQueue.offer(znak);
-                qfreespace--;
-                 if (qfreespace == 0) {//zapełnienie całego potoku tutaj powinna być synchronizacja
+                plik1.tab[index].myQueue.offer(znak);
+                plik1.tab[index].qfreespace--;
+                 if ( plik1.tab[index].qfreespace == 0) {//zapełnienie całego potoku tutaj powinna być synchronizacja
             return 2;            
             }
         }
@@ -55,16 +58,17 @@ public class Potoki {
     }
 
     //ta funkcja chyba powinna być w klasie procesu
-    void pipe(plik1 plik)//służy do utworzenia potoku
+    void pipe(process p)//służy do utworzenia potoku
     {
         //proces znajduje wolny deskryptor inicjalizuje swoje indexy deskryptora;
-        int index = plik.finddes();//od obiektu file
+        int index;//od obiektu file
+        index = plik1.finddes();
         if (index == -1) {
             System.out.println("Błąd deskryptora");
         } else {
-            //this->des[1]=index;
-            //this->next.des[0]=index;
+            p.des=index;
+            p.next.des=index;
+            plik1.tab[index].open=1;
         }
     }
-    
 }
